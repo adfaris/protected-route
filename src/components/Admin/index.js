2a4f8c62cc0cfd377a6d0e6d73aa4ae5c726b7d4
A.D.Faris <adfaris@gmail.com>
2019-02-01 18:58:19 -0500

    admin roles

--- src/components/Admin/index.js
+++ src/components/Admin/index.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import { withAuthorization } from '../Session'
+import * as  ROLES from '../../constants/roles'
 
 const Admin = () =>
   <div>
     <h1>Admin page</h1>
+    <p>
+      Ristricted area! only users with admin role are authorized
+    </p>
   </div>
 
-const condition = authUser => !!authUser
+const condition = authUser => authUser && authUser.roles.includes(ROLES.Admin)
 
 export default withAuthorization(condition)(Admin)
